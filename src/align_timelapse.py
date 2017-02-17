5ebc5b1ba192c28da0ec0e577443b1610ea97782
valentina-s <valentina-s@users.noreply.github.com>
2017-02-17 14:43:10 -0800

    fixed video writing for timelapse

--- src/align_timelapse.py
+++ src/align_timelapse.py
@@ -27,29 +27,44 @@ def sync_corr(signal1, signal2):
     offset = cc.argmax() + l - len(cc)
     return(offset)
 
-def alignTimelapse(aligned_urls):
+def readTimelapse(urls):
+    """
+        readTimelapse reads a sequence of frames from urls
+    """
+    from skimage import io
+
     video_timelapse = []
-    for url in aligned_urls:
+    for url in urls:
         try:
             video_timelapse.append(io.imread(url))
         except:
+            print('passing')
             pass
     return(video_timelapse)
 
 
 def saveTimelapse(video_timelapse,filename):
+    """
+        saveTimelapse converts a video (list of frames) into an .mp4 video
+    """
     import imageio
-    """
 
-    """
+    writer = imageio.get_writer(filename, fps=1)
 
-    writer = imageio.get_writer(filename, fps=5)
-
+    print(len(video_timelapse))
     for im in video_timelapse:
+        print('frame')
         writer.append_data(im)
     writer.close()
 
 if __name__ == '__main__':
+  """
+    Example Usage:
+
+    python align_timelapse.py --start_date 2016/05/01 --end_date 2016/05/05 --sampling DAILY --frame 5000 --subsampleRate 100 --interval 2
+
+  """
+
   import argparse
   from datetime import datetime, timedelta
   from collections import OrderedDict
@@ -59,17 +74,22 @@ if __name__ == '__main__':
   parser.add_argument('-frame','--frame',type=int,required=True)
   parser.add_argument('-end_date','--end_date',type=str,required=True)
   parser.add_argument('-sampling','--sampling',type=str,required=True)
+  parser.add_argument('-subsampleRate','--subsampleRate',type=int,required=False)
+  parser.add_argument('-interval','--interval',type=int,required=False)
   args = parser.parse_args()
 
-  list_movie_urls = generate_urls(args.start_date,args.end_date,args.sampling)
+  # generate a list of urls based on the parameters
+  list_movie_urls = generate_urls(args.start_date,args.end_date,args.sampling,args.interval)
 
+  # drop urls which are not responsive
   filtered_urls = [url for url in list_movie_urls if ping(url)]
 
-  #  calculate rolling variance
-  res = [calculateRollingStats_fromUrl(url,lag=3,subsampleRate=10,frame_start=3000,frame_end=7000) for url in filtered_urls]
+
+  #  calculate rolling variance for each url
+  res = [calculateRollingStats_fromUrl(url,lag=3,subsampleRate=args.subsampleRate,frame_start=3000,frame_end=7000) for url in filtered_urls]
   res_np = [np.array(r) for r in res]
 
-  # align all time series
+  # extract offsets of misalignment
   offsets = []
   for i in range(1,len(res_np)):
       if res_np[i].shape != ():
@@ -79,8 +99,13 @@ if __name__ == '__main__':
           offsets.append(None)
 
   offsets = [0]+offsets
-  print(offsets)
-  aligned_urls = [url+'/frame/'+str(args.frame-offset*10) for url,offset in zip(filtered_urls,offsets) if offset is not None]
-  print(aligned_urls)
-  video_timelapse = alignTimelapse(aligned_urls)
+
+
+  # aligning the urls
+  aligned_urls = [url+'/frame/'+str(args.frame-offset*args.subsampleRate) for url,offset in zip(filtered_urls,offsets) if offset is not None]
+
+  # extracting frames for the timelapse
+  video_timelapse = readTimelapse(aligned_urls)
+
+  # saving the video
   saveTimelapse(video_timelapse,'timelapse.mp4')
